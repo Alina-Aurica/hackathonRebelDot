68b00d89b6dcd144c7928a7220b00941071d9afd
Alina-Aurica <alynaa.2001@gmail.com>
2024-10-17 19:53:10 +0300

    Add models and repository

---
+++ HackathonRebelDotBackend/application/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+from application.model.models import db
+from config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
+
+    # from .controller.userController import user_controller
+    # app.register_blueprint(user_controller)
+    # from .controller.destinationController import destination_controller
+    # app.register_blueprint(destination_controller)
+    # from .controller.reservationController import reservation_controller
+    # app.register_blueprint(reservation_controller)
+    # from .controller.authController import auth_controller
+    # app.register_blueprint(auth_controller)
+
+    return app
